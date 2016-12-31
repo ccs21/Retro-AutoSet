d4df90eea5b519b8c6b1903d0d2df7198394fab5
ccs21 <ccs2121@naver.com>
2017-01-01 08:12:23 +0900

    1
    1

---
+++ 00.Install.sh
@@ -0,0 +1,12 @@
+cd /home/pi/RetroPie-AutoSet
+chmod 755 *sh
+./01.Localization.sh
+sleep 1
+./02.Hangul_Gamelist.sh
+sleep 1
+./03.TimeZone.sh
+sleep 1
+./04.Samba.sh
+sleep 1
+
+echo "Basic Setting Complete"
