7682bad27f303ea914784c3832410657ee4126cf
ccs21 <ccs2121@naver.com>
2017-01-02 03:26:21 +0900

    Update 04.Samba.sh

--- 04.Samba.sh
+++ 04.Samba.sh
@@ -1,6 +1,6 @@
 ## 07. Samba Setting(pi home directory share)
 
-AUTOSET_HOME=/home/pi/RetroPie-AutoSet
+AUTOSET_HOME=/home/pi/Retro-AutoSet
 
 cat $AUTOSET_HOME/Cab/07.Samba/smb.conf >> /etc/samba/smb.conf
 
